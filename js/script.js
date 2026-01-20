24e700819d3feeb621d725504b065941466e53ac
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-20 11:47:33 -0700

    Link JS file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-const inputs = documnt.querySelectorAll('.controls input');
+const inputs = documnt.querySelectorAll('input');
+
+function handleUpdate() {
+    console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mouseOver', handleUpdate));
